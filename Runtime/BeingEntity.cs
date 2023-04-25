ed4c132846b2fe1f51701391e4121a13d7e54d47
LAPTOP-3BPAUQ9G\Iseei <iseeicyhawkston@gmail.com>
2023-04-25 01:17:44 -0500

    Initial Commit

---
+++ Runtime/BeingEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace BobboNet.Engine
+{
+    public class BeingEntity : MonoBehaviour
+    {
+        //
+        //  Initialization
+        //
+
+        [Inject]
+        public void Inject()
+        {
+
+        }
+    }
+}
